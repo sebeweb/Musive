d566d0d1bb2880f7bd84da6bd0276c048765b844
Niels34410 <chaigneau.niels@gmail.com>
2017-05-24 09:14:37 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -3,15 +3,13 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var i;
-//Salon
-var room;
 //db
 var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database('database');
+var db = new sqlite3.Database('data/tchat.db');
 
 db.serialize(function () {
     // Database initialization
-    db.get("SELECT * FROM sqlite_master WHERE 'users' ",
+    db.get("SELECT * FROM users",
            function(err, rows) {
       if(err !== null) {
         console.log(err);
@@ -46,17 +44,20 @@ app.get('/chat/:channel', function (req, res) {
         res.redirect("/room?" + user);
     }
 });
-app.use('/room', express.static(__dirname + '/public'));
+/**
+ * Gestion des requêtes HTTP des utilisateurs en leur renvoyant les fichiers du dossier 'public'
+ */
+app.use('/', express.static(__dirname + '/public'));
 
 /**
  * Liste des utilisateurs connectés
  */
 var users = [];
 
-/*
- * L'objet messages
+/**
+ * Historique des messages
  */
-var messages = {};
+var messages = [];
 
 /**
  * Liste des utilisateurs en train de saisir un message
@@ -64,139 +65,136 @@ var messages = {};
 var typingUsers = [];
 
 io.on('connection', function (socket) {
-    /**
-     * Utilisateur connecté à la socket
-     */
-    var loggedUser;
-    socket.room = room;
-    socket.join(room);
-    /*
-     * Historique des message par salon
-     */
-    messages[room] = [];
-    /**
-     * Emission d'un événement "user-login" pour chaque utilisateur connecté
-     */
+
+  /**
+   * Utilisateur connecté à la socket
+   */
+  var loggedUser;
+
+  /**
+   * Emission d'un événement "user-login" pour chaque utilisateur connecté
+   */
+  for (i = 0; i < users.length; i++) {
+    socket.emit('user-login', users[i]);
+  }
+
+  /**
+   * Emission d'un événement "chat-message" pour chaque message de l'historique
+   */
+  for (i = 0; i < messages.length; i++) {
+    if (messages[i].username !== undefined) {
+      socket.emit('chat-message', messages[i]);
+    } else {
+      socket.emit('service-message', messages[i]);
+    }
+  }
+
+  /**
+   * Déconnexion d'un utilisateur
+   */
+  socket.on('disconnect', function () {
+    if (loggedUser !== undefined) {
+      // Broadcast d'un 'service-message'
+      var serviceMessage = {
+        text: 'Utilisateur "' + loggedUser.username + '" déconnecté',
+        type: 'logout'
+      };
+      socket.broadcast.emit('service-message', serviceMessage);
+      // Suppression de la liste des connectés
+      var userIndex = users.indexOf(loggedUser);
+      if (userIndex !== -1) {
+        users.splice(userIndex, 1);
+      }
+      // Ajout du message à l'historique
+      messages.push(serviceMessage);
+      // Emission d'un 'user-logout' contenant le user
+      io.emit('user-logout', loggedUser);
+      // Si jamais il était en train de saisir un texte, on l'enlève de la liste
+      var typingUserIndex = typingUsers.indexOf(loggedUser);
+      if (typingUserIndex !== -1) {
+        typingUsers.splice(typingUserIndex, 1);
+      }
+    }
+  });
+
+  /**
+   * Connexion d'un utilisateur via le formulaire :
+   */
+  socket.on('user-login', function (user, callback) {
+    // Vérification que l'utilisateur n'existe pas
+    var userIndex = -1;
     for (i = 0; i < users.length; i++) {
-        socket.emit('user-login', users[i]);
+      if (users[i].username === user.username) {
+        userIndex = i;
+      }
     }
-    /**
-     * Emission d'un événement "chat-message" pour chaque message de l'historique
-     */
-    for (i = 0; i < messages[room].length; i++) {
-//        if (messages[room][i].username !== undefined) {
-
-        socket.in(socket.room).emit('chat-message', messages[room][i]);
-//        } else {
-//            socket.in(socket.room).emit('service-message', messages[room][i]);
-//        }
+    if (user !== undefined && userIndex === -1) { // S'il est bien nouveau
+      // Sauvegarde de l'utilisateur et ajout à la liste des connectés
+      loggedUser = user;
+      users.push(loggedUser);
+      // Envoi et sauvegarde des messages de service
+      var userServiceMessage = {
+        text: 'Vous êtes connecté en tant que "' + loggedUser.username + '"',
+        type: 'login'
+      };
+      var broadcastedServiceMessage = {
+        text: 'Utilisateur "' + loggedUser.username + '" connecté',
+        type: 'login'
+      };
+      socket.emit('service-message', userServiceMessage);
+      socket.broadcast.emit('service-message', broadcastedServiceMessage);
+      messages.push(broadcastedServiceMessage);
+      // Emission de 'user-login' et appel du callback
+      io.emit('user-login', loggedUser);
+      callback(true);
+    } else {
+      callback(false);
     }
+  });
 
-    /**
-     * Déconnexion d'un utilisateur
-     */
-    socket.on('disconnect', function () {
-        if (loggedUser !== undefined) {
-            // Broadcast d'un 'service-message'
-//            var serviceMessage = {
-//                text: 'Utilisateur "' + loggedUser.username + '" déconnecté',
-//                type: 'logout'
-//            };
-//            socket.broadcast.in(socket.room).emit('service-message', serviceMessage);
-            // Suppression de la liste des connectés
-            var userIndex = users.indexOf(loggedUser);
-            if (userIndex !== -1) {
-                users.splice(userIndex, 1);
-            }
-            // Ajout du message à l'historique
-//            messages[room].push(serviceMessage);
-            // Emission d'un 'user-logout' contenant le user
-            io.in(socket.room).emit('user-logout', loggedUser);
-            // Si jamais il était en train de saisir un texte, on l'enlève de la liste
-            var typingUserIndex = typingUsers.indexOf(loggedUser);
-            if (typingUserIndex !== -1) {
-                typingUsers.splice(typingUserIndex, 1);
-            }
-        }
-    });
+  /**
+   * Réception de l'événement 'chat-message' et réémission vers tous les utilisateurs
+   */
+  socket.on('chat-message', function (message) {
+    // On ajoute le username au message et on émet l'événement
+    message.username = loggedUser.username;
+    io.emit('chat-message', message);
+    // Sauvegarde du message
+    messages.push(message);
+    if (messages.length > 150) {
+      messages.splice(0, 1);
+    }
+  });
 
-    /**
-     * Connexion d'un utilisateur via le formulaire :
-     */
-    socket.on('user-login', function (user, callback) {
-        // Vérification que l'utilisateur n'existe pas
-        var userIndex = -1;
-        for (i = 0; i < users.length; i++) {
-            if (users[i].username === user.username) {
-                userIndex = i;
-            }
-        }
-        if (user !== undefined && userIndex === -1) { // S'il est bien nouveau
-            // Sauvegarde de l'utilisateur et ajout à la liste des connectés
-            loggedUser = user;
-            users.push(loggedUser);
-            // Envoi et sauvegarde des messages de service
-//            var userServiceMessage = {
-//                text: 'Vous êtes connecté en tant que "' + loggedUser.username + '"',
-//                type: 'login'
-//            };
-//            var broadcastedServiceMessage = {
-//                text: 'Utilisateur "' + loggedUser.username + '" connecté',
-//                type: 'login'
-//            };
-//            socket.emit('service-message', userServiceMessage);
-//            socket.broadcast.to(socket.room).emit('service-message', broadcastedServiceMessage);
-//            messages[room].push(broadcastedServiceMessage);
-            io.emit('user-login', loggedUser);
-            callback(true);
-        } else {
-            callback(false);
-        }
-    });
+  /**
+   * Réception de l'événement 'start-typing'
+   * L'utilisateur commence à saisir son message
+   */
+  socket.on('start-typing', function () {
+    // Ajout du user à la liste des utilisateurs en cours de saisie
+    if (typingUsers.indexOf(loggedUser) === -1) {
+      typingUsers.push(loggedUser);
+    }
+    io.emit('update-typing', typingUsers);
+  });
 
-    /**
-     * Réception de l'événement 'chat-message' et réémission vers tous les utilisateurs
-     */
-    socket.on('chat-message', function (message) {
-        // On ajoute le username au message et on émet l'événement
-        message.username = loggedUser.username;
-        io.in(socket.room).emit('chat-message', message);
-        // Sauvegarde du message
-        messages[room].push(message);
-//        console.log(messages);
-        if (messages[room].length > 150) {
-            messages[room].splice(0, 1);
-        }
-    });
-
-    /**
-     * Réception de l'événement 'start-typing'
-     * L'utilisateur commence à saisir son message
-     */
-    socket.on('start-typing', function () {
-        // Ajout du user à la liste des utilisateurs en cours de saisie
-        if (typingUsers.indexOf(loggedUser) === -1) {
-            typingUsers.push(loggedUser);
-        }
-        io.in(socket.room).emit('update-typing', typingUsers);
-    });
-
-    /**
-     * Réception de l'événement 'stop-typing'
-     * L'utilisateur a arrêter de saisir son message
-     */
-    socket.on('stop-typing', function () {
-        var typingUserIndex = typingUsers.indexOf(loggedUser);
-        if (typingUserIndex !== -1) {
-            typingUsers.splice(typingUserIndex, 1);
-        }
-        io.in(socket.room).emit('update-typing', typingUsers);
-    });
+  /**
+   * Réception de l'événement 'stop-typing'
+   * L'utilisateur a arrêter de saisir son message
+   */
+  socket.on('stop-typing', function () {
+    var typingUserIndex = typingUsers.indexOf(loggedUser);
+    if (typingUserIndex !== -1) {
+      typingUsers.splice(typingUserIndex, 1);
+    }
+    io.emit('update-typing', typingUsers);
+  });
 });
 
 /**
  * Lancement du serveur en écoutant les connexions arrivant sur le port process.env.PORT
+ * process.env.PORT,
  */
-http.listen(process.env.PORT || 5000, function () {
-    console.log('Server is listening on process.env.PORT');
+http.listen(3000, function () {
+  console.log('Server is listening on *:3000');
 });
